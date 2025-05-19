d2065828cb8e6463ca30a9c6e4250ead55fa7994
Krish Balana <168128787+Krrishbalana@users.noreply.github.com>
2025-05-19 21:07:55 +0530

    238. Product of Array Except Self

--- 238.cpp
+++ 238.cpp
@@ -37,3 +37,30 @@ public:
         return ans;
     }
 };
+
+
+// approach with better time and space complexity
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        // vector<int> prefix(n, 1);
+        // vector<int> suffix(n, 1);
+        int prefix = 1;
+        int suffix = 1;
+        //prefix => ans
+        for(int i = 1; i < n; i++){
+            ans[i] = ans[i-1] * nums[i-1]; 
+        }
+
+        //suffix
+        for(int i = n-2; i >= 0; i--){
+            suffix *= nums[i+1];
+            ans[i] = suffix * ans[i];
+        }
+
+
+        return ans;
+    }
+};
